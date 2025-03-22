ead17963717181b39598e51bf0383b613d6b2efc
CHenriqueRibeiro <chmr66@gmail.com>
2025-03-22 18:21:53 -0300

    ajuste no retorno da requisição do login

--- backend/controllers/ownerController.js
+++ backend/controllers/ownerController.js
@@ -1,6 +1,6 @@
-const Owner = require('../models/Owner');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
+const Owner = require("../models/Owner");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const SECRET_KEY = process.env.SECRET_KEY;
 
 exports.registerOwner = async (req, res) => {
@@ -9,7 +9,7 @@ exports.registerOwner = async (req, res) => {
   try {
     const existingOwner = await Owner.findOne({ email });
     if (existingOwner) {
-      return res.status(400).json({ message: 'Dono já cadastrado' });
+      return res.status(400).json({ message: "Email já cadastrado" });
     }
 
     const salt = await bcrypt.genSalt(10);
@@ -25,19 +25,21 @@ exports.registerOwner = async (req, res) => {
 
     await newOwner.save();
 
-    const token = jwt.sign({ id: newOwner._id }, SECRET_KEY, { expiresIn: '1h' });
+    const token = jwt.sign({ id: newOwner._id }, SECRET_KEY, {
+      expiresIn: "1h",
+    });
 
     res.status(201).json({
       owner: {
         id: newOwner._id,
         name: newOwner.name,
         email: newOwner.email,
-        phone: newOwner.phone
+        phone: newOwner.phone,
       },
       token,
     });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: 'Erro ao criar o dono' });
+    res.status(500).json({ message: "Erro ao criar o dono" });
   }
 };
